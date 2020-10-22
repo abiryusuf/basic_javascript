e81ef71db8e28de33b563789efb14b2b2e270374
Abir Yusuf <abiryusuf@abirs-imac.home>
2020-10-22 00:42:05 -0400

    map

--- 13_Basic/countdown.js
+++ 13_Basic/countdown.js
@@ -1,3 +1,5 @@
+// Write code to print all numbers from `num` down to 1
+// You may assume `num` will be a positive number
 const countDown = function(num){
     for(var i = num; i>0; i--){
         console.log(i)
