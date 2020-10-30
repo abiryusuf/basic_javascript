05763446cc939d14e54e93752c526ef3636305a4
Abir Yusuf <yusuf_ctg@yahoo.com>
2020-10-30 00:18:24 -0400

    reduce

---
+++ 13_Basic/reduce.js
@@ -0,0 +1,18 @@
+const num = [1,2,3,4,5];
+
+const reducer = (num1, num2 ) => num1 * num2;
+
+console.log("reduce", num.reduce(reducer));
+
+function factorialDemo(num){
+    var factorial = 1;
+    for (var i = 0; i < num.length; i++) {
+        //const ele = num[i];
+        factorial += factorial * i;
+        
+    }
+    return factorial;
+}
+var str = factorialDemo(num);
+console.log("factorial", str);
+
