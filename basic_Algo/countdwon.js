e4871d174664208d712d4b6c0b51cc5bc618a524
Abir Yusuf <yusuf_ctg@yahoo.com>
2020-11-02 23:53:46 -0500

    countdown

---
+++ basic_Algo/countdwon.js
@@ -0,0 +1,9 @@
+//write code to print all numbers from num down to 1
+// you may assume num will be a positive number
+
+const countDown = function(str){
+    for(var i = 10; i>0; i--){
+        console.log(i)
+    }
+}
+countDown();
