e11cd82be286ba47f1b11071f101a1a51faf3651
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-01-23 17:38:08 -0500

    call function

--- JavaScript1/function/call.js
+++ JavaScript1/function/call.js
@@ -9,12 +9,12 @@ var person2 = {
 }
 
 var person = {
-    fullName: function(){
-        return this.name + " " + this.place;
+    fullName: function(age, color){
+        return this.name + " " + this.place + "," + age + "," + color;
     }
 }
-
-var result = person.fullName.call(person1);
+// when too many objects belongs to one method, then we use the "call" method 
+var result = person.fullName.call(person1, 30, "red");
 
 console.log(result);
 
