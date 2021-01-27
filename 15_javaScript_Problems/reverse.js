44c3ea547e1eb5552a87ab592471b3f5fa0d33db
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-01-26 22:09:34 -0500

    string

--- 15_javaScript_Problems/reverse.js
+++ 15_javaScript_Problems/reverse.js
@@ -1,12 +1,25 @@
-function reverseString(str){
-    var reverse = "";
-    for(var i = 0; i<str.length; i++){
-        var char = str[i];
-        reverse = char + reverse;
-    }
-    return reverse;
-}
-var info = "I am abir";
-var result = reverseString(info);
-console.log(result);
+// function reverseString(str){
+//     var reverse = "";
+//     for(var i = 0; i<str.length; i++){
+//         var char = str[i];
+//         reverse = char + reverse;
+//     }
+//     return reverse;
+// }
+// var info = "I am abir";
+// var result = reverseString(info);
+// console.log(result);
 
+function reverseString(str){
+    var reverse = str.split("");
+    var res = "";
+    for (let i = 0; i < reverse.length; i++) {
+        const element = reverse[i];
+        var res1 = element + res;
+
+        //console.log(element);
+        
+    }
+    return res1;
+}
+console.log(reverseString("abir"));
