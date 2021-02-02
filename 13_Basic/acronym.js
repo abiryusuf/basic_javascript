48a1acff32b3d1f8a537a7a25001f79e18d86177
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-01 21:58:57 -0500

    acronym

--- 13_Basic/acronym.js
+++ 13_Basic/acronym.js
@@ -1,8 +1,25 @@
 // Write code to create a function that accepts a string and returns an acronym for the given string
 
-const acronymBuilder = function(str){
+// const acronymBuilder = function(str){
+//     var words = str.split(" ");
+//     var result = "";
+
+//     for (let i = 0; i < words.length; i++) {
+//         const element = words[i];
+
+//         result += element[0].toUpperCase();
+        
+//     }
+//     return result;
+// }
+
+// var info = "abir yusuf"
+// var res = acronymBuilder(info);
+// console.log(res);
+
+function acronym1(str){
     var words = str.split(" ");
-    var result = "";
+    var result ="";
 
     for (let i = 0; i < words.length; i++) {
         const element = words[i];
@@ -12,7 +29,4 @@ const acronymBuilder = function(str){
     }
     return result;
 }
-
-var info = "abir yusuf"
-var res = acronymBuilder(info);
-console.log(res);
+console.log(acronym1("abir yusuf"))
