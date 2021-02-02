b119e1876b2aaa9fc4afdbc1bb75a77594b5faa3
Abir Yusuf <yusuf_ctg@yahoo.com>
2021-02-01 22:44:58 -0500

    string method

--- JavaScript1/string/String.js
+++ JavaScript1/string/String.js
@@ -7,3 +7,13 @@ var name = 'Abir';
 
 var info = "I am abir Yusuf";
 console.log(info.indexOf("abir"));
+
+var str = "apple, Banana, KiWi";
+console.log(str.slice(7, 13))
+
+var text1 = "Abir";
+var text2 = "Yusuf";
+
+var res = text1.concat(" ", text2);
+var res1 = text2 + " " + text1;
+console.log(res1);
