7d2baca286fb4dad545ae05091cd7bc208502269
abiryusuf <yusuf_ctg@yahoo.com>
2021-02-26 00:03:34 -0500

    max update

--- basic_Algo/max.js
+++ basic_Algo/max.js
@@ -1,15 +1,15 @@
 
 var maxNumber = function(num){
-    var max = [];
-    for (let i = 0; i < num.length; i++) {
-        const currentNum = num[i];
-
-        if (currentNum > max) {
-            max = currentNum;
-        }
-        
-    }
-    return max;
+    var max = 0;
+  for (let i = 0; i < num.length; i++) {
+      const currentNum= num[i];
+      //console.log(currentNum)
+      if (currentNum > max) {
+          max = currentNum;
+      }
+      
+  }
+  return max;
 }
-var number = [4,5,7,8,3,10]
+var number = [4,5,7,8,3,10,70]
 console.log(maxNumber(number))
